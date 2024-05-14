c55bf1373be606732736f9cfb1f7f8b5451ff38e
samihaAbdel <samKallel@hotmail.fr>
2024-05-14 19:09:57 +0100

    tout le projet

--- src/Components/Posts/DisplayPosts.js
+++ src/Components/Posts/DisplayPosts.js
@@ -1,38 +1,51 @@
 import React, { useEffect } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import { useDispatch, useSelector } from "react-redux";
 import { deletePost, getAllPosts } from "../../Redux/actions/post";
 import "./style.css";
 import { Link, useNavigate } from "react-router-dom";
-function DisplayPosts () {
-    const dispatch = useDispatch();
-    const navigate = useNavigate()
-    useEffect(() => {
-        dispatch(getAllPosts())
-    },[dispatch])
-    const posts = useSelector((state) => state.postReducer.posts);
-    const handleDelete =(id)=>{
-        dispatch(deletePost(id))
-        navigate('/')
-    }
-    return (
-        <div className="pub-card">
-            {posts && posts.map((p) =>
-                <Card className="card" style={{ width: '18rem' }}>
-                    <Card.Img style ={{ height:'170px'}} variant="top" src={p.LinkImage} />
-                    <Card.Body>
-                        <Card.Title>{p.Title}</Card.Title>
-                        <Card.Text>
-                            {p.Description}
-                        </Card.Text>
-                        
-                        <Button variant="outline-secondary" onClick={()=>handleDelete(p._id)}>delete</Button>
-                        
-                        <Button variant="outline-secondary">Update</Button>
-                    </Card.Body>
-                </Card>)}
-        </div>
-    )
+function DisplayPosts() {
+  const isAuth = useSelector((state) => state.userReducer.isAuth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+  const posts = useSelector((state) => state.postReducer.posts);
+  const handleDelete = (id) => {
+    dispatch(deletePost(id));
+    navigate("/");
+  };
+  return (
+    <div className="pub-card">
+      {posts &&
+        posts.map((p) => (
+          <Card className="card" style={{ width: "18rem" }}>
+            <Card.Img
+              style={{ height: "170px" }}
+              variant="top"
+              src={p.LinkImage}
+            />
+            <Card.Body>
+              <Card.Title>{p.Title}</Card.Title>
+              <Card.Text>{p.Description}</Card.Text>
+              {isAuth ? (
+                <div style={{display:'flex', justifyContent:'space-evenly'}}>
+                  <Button
+                    variant="outline-secondary"
+                    onClick={() => handleDelete(p._id)}
+                  >
+                    delete
+                  </Button>
+
+                  <Button variant="outline-secondary">Update</Button>
+                </div>
+              ) : null}
+            </Card.Body>
+          </Card>
+        ))}
+    </div>
+  );
 }
 export default DisplayPosts;
